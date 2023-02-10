ef1ddfc4cd696c8f10cc757ec8bddcbc4b2ec1de
Moustaph <103828108+Moustaph0@users.noreply.github.com>
2023-02-10 20:57:11 +0100

    Update myStr.js

--- myStr.js
+++ myStr.js
@@ -1,10 +1,2 @@
-/*
-Exercice 00 : myStr
-Construisez myStr à partir des chaînes de caractères "Ceci est le début". 
-et "Ceci est la fin". en utilisant l'opérateur +. Veillez à inclure un espace 
-entre les deux chaînes de caractères 
-*/
-
-let chaine1 = "This is the start";
-let chaine2 = "This is the end";
-console.log(chaine1 + " " + chaine2);
+let myStr ="This is the start" + " This is the end";
+console.log(myStr);
