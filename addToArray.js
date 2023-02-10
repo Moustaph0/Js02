bbdc2eb6c62b365a497624237e886490dc3010df
Moustaph <103828108+Moustaph0@users.noreply.github.com>
2023-02-10 20:04:22 +0100

    Update addToArray.js

--- addToArray.js
+++ addToArray.js
@@ -1,10 +1,3 @@
-/*
-Exercice 04: addToArray
-• soit :
-const myArray = [["John", 23], ["cat", 2]];
-• Utiliser Push pour inserer ["Cs", 1]
-
-*/
 const myArray = [
     ["John", 23],
     ["cat", 2]
