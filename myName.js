31784f45a68fc631df6bcc582ec04a550a7369d9
Moustaph <103828108+Moustaph0@users.noreply.github.com>
2023-02-10 20:48:39 +0100

    Update myName.js

--- myName.js
+++ myName.js
@@ -1,9 +1,2 @@
-/*
-Exercice 01 : myName
-Définissez myName comme une chaîne caractère égale à «Daouda » et 
-construisez myStr avec myName entre les chaînes ‘’My name is ‘’ ‘’and I 
-am well!’’
-*/
-
-let myName = "Daouda";
+let myName = "Moustapha";
 console.log(`My name is ${myName} and I am well!`);
